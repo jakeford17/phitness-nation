59912aedc4b837bf7890c38959499748abc76259
Cassidy Foust <cassidy.foust@gmail.com>
2019-11-12 09:35:52 -0600

    added goal modal

--- src/components/AddGoalModal/AddGoalModal.js
+++ src/components/AddGoalModal/AddGoalModal.js
@@ -6,6 +6,30 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Select from '@material-ui/core/Select';
+import { styled } from '@material-ui/core/styles';
+
+const MySelect = styled(Select)({
+    // background: '#F1EDBF',
+    // border: 0,
+    // borderRadius: 3,
+    padding: 10,
+    margin: 5,
+    textAlign: "center",
+    width: "75%"
+});
+
+const MyTextField = styled(TextField)({
+    // background: '#F1EDBF',
+    // border: 0,
+    // borderRadius: 3,
+    padding: 10,
+    margin: 5,
+    width: "75%",
+    textAlign: "center"
+});
 
 export default function FormDialog() {
     const [open, setOpen] = React.useState(false);
@@ -18,33 +42,52 @@ export default function FormDialog() {
         setOpen(false);
     };
 
+    const [values, setValues] = React.useState({
+        goalLength: '',
+        goalDescription: '',
+    });
+
+    const handleChange = name => event => {
+        setValues({ ...values, [name]: event.target.value });
+    };
+
     return (
         <div>
             <Button variant="outlined" color="primary" onClick={handleClickOpen}>
                 Add Goal
             </Button>
             <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+                <DialogTitle id="form-dialog-title">Add Goal</DialogTitle>
                 <DialogContent>
                     <DialogContentText>
-                        To subscribe to this website, please enter your email address here. We will send updates
-                        occasionally.
+                        <MySelect
+                            value={values.goalLength}
+                            onChange={handleChange('goalLength')}
+                        >
+                            <MenuItem value="">
+                                <em></em>
+                            </MenuItem>
+                            <MenuItem value={"short term"}>Short Term</MenuItem>
+                            <MenuItem value={"long term"}>Long Term</MenuItem>
+                        </MySelect>
+                        <FormHelperText>Short term or long term goal?</FormHelperText>
+                        <MyTextField
+                            label="Description"
+                            value={values.goalDescription}
+                            multiline
+                            rowsMax="4"
+                            onChange={handleChange('goalDescription')}
+                            margin="normal"
+                        />
+                        <FormHelperText>Add a short description of your goal.</FormHelperText>
           </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Email Address"
-                        type="email"
-                        fullWidth
-                    />
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose} color="primary">
                         Cancel
           </Button>
                     <Button onClick={handleClose} color="primary">
-                        Subscribe
+                        Save
           </Button>
                 </DialogActions>
             </Dialog>
