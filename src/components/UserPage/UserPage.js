e6c51cff40aa695a10723d9ef4c8332948557d4c
Cassidy Foust <cassidy.foust@gmail.com>
2019-11-12 16:09:19 -0600

    added streak bar from bootstrap

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
+import './UserPage.css';
+import { styled } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import { flexbox } from '@material-ui/system';
+import {ProgressBar} from 'react-bootstrap'
+
+const MyCard = styled(Card)({
+  background: 'transparent',
+  border: 0,
+  borderRadius: 3,
+  // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  // // color: 'white',
+  // height: 60,
+  width: "90%",
+  padding: 10,
+  margin: 5,
+  fontSize: 16,
+  display: flexbox,
+  textAlign: "center"
+});
 
 class UserPage extends Component{
   componentDidMount = () =>{
@@ -11,23 +31,30 @@ class UserPage extends Component{
   }
   render() {
     return(
-      <div>
+      <>
+      <div className="user-page">
         <h1 >
           Welcome, { this.props.reduxState.user.name }!
         </h1>
         <h2>
-          Weekly Philosophy: { this.props.reduxState.user.philosophy }
+          Weekly PHILosophy: 
         </h2>
+          <MyCard>
+            {this.props.reduxState.user.philosophy}
+          </MyCard>
         <h3>
-          Streak: [#######----]
+          Streak:
+          <div className="streak">
+              <ProgressBar striped variant="success" now={this.props.reduxState.user.current_streak} />
+            </div>
         </h3>
 
         <LogOutButton className="log-in" />
       </div>
+      </>
     )
-
   }
-};
+}
 
 // Instead of taking everything from state, we just want the user info.
 // if you wanted you could write this code like this:
