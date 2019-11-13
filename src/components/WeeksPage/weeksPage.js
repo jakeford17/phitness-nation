0616fa8c3939c593445ad25c5097b8d606226c1f
Cassidy Foust <cassidy.foust@gmail.com>
2019-11-13 13:02:28 -0600

    appends user workouts to dom

--- src/components/WeeksPage/weeksPage.js
+++ src/components/WeeksPage/weeksPage.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './weeksPage.css';
+import { styled } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import { flexbox } from '@material-ui/system';
+
+
+const MyCard = styled(Card)({
+  background: '#d2d2d4',
+  border: 0,
+  borderRadius: 3,
+  // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  // // color: 'white',
+  // height: 60,
+  width: "90%",
+  padding: 10,
+  margin: 5,
+  fontSize: 20,
+  display: flexbox,
+  textAlign: "left"
+});
 
 class  UserDashboard extends Component {
+
+  componentDidMount = () => {
+    this.props.dispatch({type: 'FETCH_WORKOUTS'})
+  }
 
   render() {
     return (
       <div className="weeks-page">
         <h1>Workouts by Week</h1>
-        <button onClick ={() => alert("redirect to this weeks workouts")}>Week 1</button>
-        <button onClick ={() => alert("redirect to this weeks workouts")}>Week 2</button>
-        <button onClick ={() => alert("redirect to this weeks workouts")}>Week 3</button>
+        {this.props.reduxState.workouts.workoutsReducer.map((workout) => {
+            return (
+              <MyCard onClick={() => alert("redirect to this weeks workouts")}>
+                <div>
+                  <h4>Week {workout.week}</h4>
+                </div>
+              </MyCard>
+            )
+          })}
       </div>
     );
   }
