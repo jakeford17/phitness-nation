fd4a687044ea5d7caa2d0195bf3ed32e0defc419
Cassidy Foust <cassidy.foust@gmail.com>
2019-11-13 14:53:41 -0600

    appends workouts to page if they match the week id

--- src/components/WorkoutsPage/WorkoutsPage.js
+++ src/components/WorkoutsPage/WorkoutsPage.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { styled } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import { flexbox } from '@material-ui/system';
+
+
+const MyCard = styled(Card)({
+  background: '#d2d2d4',
+  border: 0,
+  borderRadius: 3,
+  // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  // // color: 'white',
+  // height: 60,
+  width: "100%",
+  padding: 10,
+  margin: 5,
+  fontSize: 100,
+  display: flexbox,
+  textAlign: "left",
+});
 
 class  WorkoutsPage extends Component {
   
+  componentDidMount = () => {
+    this.props.dispatch({ type: 'FETCH_WORKOUTS' });
+  }
 
   render() {
     return (
       <div>
-        <h1>Your Workouts For Week 1</h1>
-        <button onClick ={() => alert("redirect to this workouts exercises")}>Workout 1</button>
-        <button onClick ={() => alert("redirect to this workouts exercises")}>Workout 2</button>
-        <button onClick ={() => alert("redirect to this workouts exercises")}>Workout 3</button>
+        <h1>Your Workouts For Week {this.props.match.params.id}</h1>
+        {this.props.reduxState.workouts.workoutsReducer.map((workout) => {
+          if (workout.week == this.props.match.params.id){
+          return (
+            <MyCard className="workout-weeks" onClick={() => alert("redirect to this workouts exercises")}>
+              <div>
+                <h4>Workout {workout.id}</h4>
+              </div>
+            </MyCard>
+          )
+          }
+        })
+        }
       </div>
     );
   }
