c8524560fb7f53d5b797752aa09420142bb110f2
Cassidy Foust <cassidy.foust@gmail.com>
2019-11-15 14:17:58 -0600

    changed new user to modal

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -21,8 +21,8 @@ router.post('/register', (req, res, next) => {
 
   const queryText = 'INSERT INTO "user" (username, password) VALUES ($1, $2) RETURNING id';
   pool.query(queryText, [username, password])
-    .then(() => res.sendStatus(201))
-    .catch(() => res.sendStatus(500));
+    .then(() => {res.send(req.body)})
+    .catch((e) => res.sendStatus(500));
 });
 
 // Handles login form authenticate/login POST
--- src/components/AdminUserList/AdminUserList.js
+++ src/components/AdminUserList/AdminUserList.js
@@ -7,6 +7,10 @@ import { withRouter } from 'react-router-dom';
 import { styled } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import { flexbox } from '@material-ui/system';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const MyCard = styled(Card)({
     background: '#d2d2d4',
@@ -48,8 +52,35 @@ class AdminUserList extends Component {
 
     state = {
         listUser: [],
+        newUserOpen: false,
+        username: '',
+        password: '',
     }
 
+
+    addUser = (event) => {
+        event.preventDefault();
+
+        if (this.state.username && this.state.password) {
+            this.props.dispatch({
+                type: 'ADD_USER',
+                payload: {
+                    username: this.state.username,
+                    password: this.state.password,
+                },
+            })
+            this.handleNewUserClose();
+            // this.props.history.push('/adminviewuser');
+        } else {
+            this.props.dispatch({ type: 'REGISTRATION_INPUT_ERROR' });
+        }
+    } // end registerUser
+
+    handleInputChangeFor = propertyName => (event) => {
+        this.setState({
+            [propertyName]: event.target.value,
+        });
+    }
     componentDidMount() {
         this.listUsers();
     }
@@ -57,15 +88,25 @@ class AdminUserList extends Component {
     listUsers = () => {
         axios.get('/api/admin').then((response) => {
             console.log("grabbing user list:", response.data)
-            this.setState({
+            this.setState({...this.state,
                 listUser: response.data
             })
         })
     }
 
-    addUserBtn = () => {
+    handleNewUserOpen = () => {
+        this.setState({...this.state,
+        newUserOpen: true})
+    }
+
+    handleNewUserClose = () => {
+        this.setState({
+            ...this.state,
+            newUserOpen: false
+        });
+        this.listUsers();
         this.props.dispatch({ type: 'SET_TO_ADD_USER_MODE' });
-        this.props.history.push('/adminadduser')
+
     }
 
     fetchClientID = (event) => {
@@ -90,9 +131,71 @@ class AdminUserList extends Component {
                     }
                 })}
                 <div className="add-client-wrapper">
-                <Fab style={styles.palette} aria-label="Add" onClick={() => this.addUserBtn()}>
-                    <AddIcon color={styles.palette.color} size="large" />
-                </Fab>
+                    <Fab style={styles.palette} aria-label="Add" onClick={() => this.handleNewUserOpen()}>
+                        <AddIcon color={styles.palette.color} size="large" />
+                    </Fab>
+                    <Dialog open={this.state.newUserOpen} onClose={this.handleNewUserClose}>
+                        <DialogTitle id="form-dialog-title"><h1>Add New User:</h1></DialogTitle>
+                        <DialogContent>
+                            {this.props.errors.registrationMessage && (
+                                <h2
+                                    className="alert"
+                                    role="alert"
+                                >
+                                    {this.props.errors.registrationMessage}
+                                </h2>
+                            )}
+                            <form onSubmit={this.addUser}>
+                                <div>
+                                    <label htmlFor="username">
+                                        Name:
+              <input
+                                            type="text"
+                                            name="username"
+                                            value={this.state.username}
+                                            onChange={this.handleInputChangeFor('username')}
+                                            className="newUserInput"
+                                        />
+                                    </label>
+                                </div>
+                                <div>
+                                    <label htmlFor="password">
+                                        Password:
+              <input
+                                            type="password"
+                                            name="password"
+                                            value={this.state.password}
+                                            onChange={this.handleInputChangeFor('password')}
+                                            className="newUserInput"
+                                        />
+                                    </label>
+                                </div>
+                                <div>
+                                    <input
+                                        className="adduser"
+                                        type="submit"
+                                        name="submit"
+                                        value="ADD USER"
+                                        className="newUserBtns"
+                                    />
+                                    <input
+                                    type="button"
+                                    value="CANCEL"
+                                    className="newUserBtns"
+                                    onClick={(e) => this.handleNewUserClose()}
+                                    />
+                                </div>
+                            </form>
+                        </DialogContent>
+                        {/* <DialogActions>
+                            <button onClick={this.handleNewUserClose}>
+                                CANCEL
+                                        </button>
+                            <button onClick={this.handleSubmit}>
+                                YES
+                                        </button>
+                        </DialogActions> */}
+                    </Dialog>
             </div>
             </div>
         );
@@ -100,7 +203,8 @@ class AdminUserList extends Component {
 }
 
 const mapStateToProps = state => ({
-    user: state.user
+    user: state.user,
+        errors: state.errors,
 });
 
 export default withRouter(connect(mapStateToProps)(AdminUserList));
