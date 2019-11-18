602c9c6701544947fa26f6031d242fcba243a126
Angela Mattson <angelamattson@Davids-MacBook-Pro-3.local>
2019-11-18 13:25:43 -0600

    username sources to name in DB and philosophy button = +

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -19,7 +19,7 @@ router.post('/register', (req, res, next) => {
   const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
 
-  const queryText = 'INSERT INTO "user" (username, password) VALUES ($1, $2) RETURNING id';
+  const queryText = 'INSERT INTO "user" (username, password, name) VALUES ($1, $2, $1) RETURNING id';
   pool.query(queryText, [username, password])
     .then(() => {res.send(req.body)})
     .catch((e) => res.sendStatus(500));
